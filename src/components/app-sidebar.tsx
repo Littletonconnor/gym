f00ce0489246ece4d4da09af5899679a2dd0f5c8
Connor Littleton <connorlittleton@wealthfront.com>
2025-04-18 17:59:52 -0700

    init

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,70 @@
+"use client"
+
+import * as React from "react"
+import {
+  IconChartBar,
+  IconDashboard,
+  IconFolder,
+  IconInnerShadowTop,
+  IconListDetails,
+} from "@tabler/icons-react"
+
+import { NavMain } from "@/components/nav-main"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+const data = {
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "/dashboard",
+      icon: IconDashboard,
+    },
+    {
+      title: "Workouts",
+      url: "/workouts",
+      icon: IconListDetails,
+    },
+    {
+      title: "History",
+      url: "/history",
+      icon: IconChartBar,
+    },
+    {
+      title: "Settings",
+      url: "/settings",
+      icon: IconFolder,
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:!p-1.5"
+            >
+              <a href="/dashboard">
+                <IconInnerShadowTop className="!size-5" />
+                <span className="text-base font-semibold">Gym App.</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+    </Sidebar>
+  )
+}
